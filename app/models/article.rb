66d988d52a32c86ec188f9827a41fdbcfbc5b712
Nathan Specht <nathanspecht@gmail.com>
2015-08-18 18:03:50 -0700

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,18 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
   def find
     @article = Article.find(params[:id])
+  end
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").map{ |s| s.strip.downcase }.uniq
+    new_or_found_tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
   end
 end
